5e355a132f7c03842dd2e1be6a8801d3c73a2559
IuryMikael <mikaeliury@gmail.com>
2024-01-30 07:10:51 -0300

    feat: create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuCardComponent } from './components/menu-card/menu-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuCardComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
