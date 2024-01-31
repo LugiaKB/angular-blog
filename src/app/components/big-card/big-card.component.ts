56dfe0ce253a6a5c195974591ccb9e15253e4f08
IuryMikael <mikaeliury@gmail.com>
2024-01-31 05:37:27 -0300

    feat: progress in stylization of big card component

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-big-card',
-  templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+    selector: 'app-big-card',
+    templateUrl: './big-card.component.html',
+    styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
-
+    @Input()
+    bigTitle: string = '';
 }
