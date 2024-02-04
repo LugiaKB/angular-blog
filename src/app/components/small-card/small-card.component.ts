d28b854f2a5220fa54c8eb7cbe99da61c42015f2
IuryMikael <mikaeliury@gmail.com>
2024-02-04 09:37:06 -0300

    feat: centralize home page content and dynamize image sources in components

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 export class SmallCardComponent {
     @Input()
     title: string = '';
+
+    @Input()
+    imageSource =
+        'https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png';
 }
