639835fda3181606e221e25911fba897f0abc558
IuryMikael <mikaeliury@gmail.com>
2024-02-04 09:02:04 -0300

    feat: base draft for small card completed

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class BigCardComponent {
     @Input()
-    bigTitle: string = '';
+    title: string = '';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-small-card',
-  templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+    selector: 'app-small-card',
+    templateUrl: './small-card.component.html',
+    styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
-
+    @Input()
+    title: string = '';
 }
