2ad6eeab4eb05b826bf36bd25ff45bb100817bb4
IuryMikael <mikaeliury@gmail.com>
2024-02-04 11:30:17 -0300

    feat: base draft for menu bar completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuCardComponent } from './components/menu-card/menu-card.component';
+
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuCardComponent,
-    BigCardComponent,
-    SmallCardComponent,
-    MenuTitleComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BigCardComponent,
+        SmallCardComponent,
+        MenuTitleComponent,
+        HomeComponent,
+        MenuBarComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
